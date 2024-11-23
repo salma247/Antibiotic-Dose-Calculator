db3c74499c3fa26a8675e20f5f60c78fde6380b0
Salma Ahmed <salma_ahmed@icloud-ready.co>
2024-11-23 17:22:23 +0200

    add homepage and base path for deployment; include deploy script in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Antibiotic-Dose-Calculator/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
